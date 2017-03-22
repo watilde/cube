aa60ef1c3df9fe7688489ba06ecd75a5b2b6b586
Daijiro Wachi <daijiro.wachi@gmail.com>
2017-03-22 18:51:16 +0100

    use global.requestAnimationFrame

--- dist/index.js
+++ dist/index.js
@@ -1,29 +1,30 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function (global){
 var THREE = require('three')
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(50, 500 / 400, 0.1, 1000);
+var scene = new THREE.Scene()
+var camera = new THREE.PerspectiveCamera(50, 500 / 400, 0.1, 1000)
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(500, 400);
-document.body.appendChild(renderer.domElement);
+var renderer = new THREE.WebGLRenderer()
+renderer.setSize(500, 400)
+document.body.appendChild(renderer.domElement)
 
-var geometry = new THREE.SphereGeometry(3, 50, 50, 0, Math.PI * 2, 0, Math.PI * 2);
-var material = new THREE.MeshNormalMaterial();
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+var geometry = new THREE.SphereGeometry(3, 50, 50, 0, Math.PI * 2, 0, Math.PI * 2)
+var material = new THREE.MeshNormalMaterial()
+var cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
 
-
-camera.position.z = 10;
+camera.position.z = 10
 var render = function () {
-    requestAnimationFrame(render);
+  global.requestAnimationFrame(render)
 
-    cube.rotation.y += 0.01;
+  cube.rotation.y += 0.01
 
-    renderer.render(scene, camera);
-};
+  renderer.render(scene, camera)
+}
 
-render();
+render()
 
+}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"three":2}],2:[function(require,module,exports){
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
